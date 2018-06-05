12206b52c8f61847fa49cc66eacf8737f19ce888
sstefan1 <sstipanovic@s-energize.com>
2018-06-05 16:57:20 +0200

    cmake

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sstefan/Desktop/gtest
-# Build directory: /Users/sstefan/Desktop/gtest/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(gtest_ex "/Users/sstefan/Desktop/gtest/build/gtest_ex")
