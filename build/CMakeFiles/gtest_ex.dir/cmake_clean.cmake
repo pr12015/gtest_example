2edb9c0aa1924788f3af90b7931624f7ff98b0bf
sstefan1 <sstipanovic@s-energize.com>
2018-06-05 16:56:13 +0200

    stuff

---
+++ build/CMakeFiles/gtest_ex.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtest_ex.dir/gtest_example.cpp.o"
+  "gtest_ex.pdb"
+  "gtest_ex"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gtest_ex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
